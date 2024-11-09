f5ae45071f0b2cafd84faf3b7e5b5797f1310d1f
nsenno-dbr <nsenno-dbr>
2024-11-09 03:25:50 +0000

    test pip build on serverless

---
+++ notebooks/test_pip.py
@@ -0,0 +1,29 @@
+# Databricks notebook source
+# MAGIC %pip install -e ../
+# MAGIC %restart_python
+
+# COMMAND ----------
+
+import pip_demo
+
+# COMMAND ----------
+
+# MAGIC %pip install ../
+# MAGIC %restart_python
+
+# COMMAND ----------
+
+import pip_demo
+
+# COMMAND ----------
+
+# MAGIC %pip install -e /Workspace/Users/nick.senno@databricks.com/pip_demo
+# MAGIC %restart_python
+
+# COMMAND ----------
+
+import pip_demo
+
+# COMMAND ----------
+
+
